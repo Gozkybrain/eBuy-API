d3aecd11793aeb35f29e0b9cb3795d231efaacfd
Gee Brain <admin@gozkybrain.com.ng>
2024-08-15 22:44:39 +0100

    Cors Added for Vite and Create React App

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const functions = require('firebase-functions');
 const express = require('express');
-const bodyParser = require('body-parser'); // Import body-parser module
-const data = require('./list.json'); // Update the path to list.json
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Import the cors module
+const data = require('./list.json');
 
 const app = express();
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
+
+// Set up CORS to allow requests from specific origins
+app.use(cors({
+    origin: [
+        'http://localhost:3000', // Allow requests from localhost:3000
+        'http://localhost:5173', // Vite default development server port
+        'https://gozkybrain.github.com' // Add your production domain here
+    ]
+}));
 
 // Route handler for the root path ("/") to display all properties
 app.get('/', (req, res) => {
@@ -16,21 +26,21 @@ app.get('/', (req, res) => {
 // Route to get properties by type
 app.get('/properties/type/:type', (req, res) => {
     const { type } = req.params;
-    const propertiesByType = data.filter(property => property.type.toLowerCase() === type.toLowerCase()); // Make comparison case-insensitive
+    const propertiesByType = data.filter(property => property.type.toLowerCase() === type.toLowerCase());
     res.json(propertiesByType);
 });
 
 // Route to get properties by location
 app.get('/properties/location/:location', (req, res) => {
     const { location } = req.params;
-    const propertiesByLocation = data.filter(property => property.location.toLowerCase() === location.toLowerCase()); // Make comparison case-insensitive
+    const propertiesByLocation = data.filter(property => property.location.toLowerCase() === location.toLowerCase());
     res.json(propertiesByLocation);
 });
 
 // Route to get properties by type and location
 app.get('/properties/:type/:location', (req, res) => {
     const { type, location } = req.params;
-    const propertiesByTypeAndLocation = data.filter(property => property.type.toLowerCase() === type.toLowerCase() && property.location.toLowerCase() === location.toLowerCase()); // Make comparison case-insensitive
+    const propertiesByTypeAndLocation = data.filter(property => property.type.toLowerCase() === type.toLowerCase() && property.location.toLowerCase() === location.toLowerCase());
     res.json(propertiesByTypeAndLocation);
 });
 
@@ -47,7 +57,7 @@ app.get('/properties/price-below-500k', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000; // Use the specified port or default to 3000
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
